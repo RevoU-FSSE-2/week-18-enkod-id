da1304e7d4e49c6cec79d60d6472ffa6a369868c
enkod-id <natanaeltamto@gmail.com>
2023-12-23 23:22:26 +0700

    revisi assignment week 18

--- be/src/validations/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.todoValidation = require('./todo.validation');
-module.exports.productValidation = require('./product.validation');
-module.exports.transactionValidation = require('./transaction.validation');
-module.exports.historyValidation = require('./history.validation');
